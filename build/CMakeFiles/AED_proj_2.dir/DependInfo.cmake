432b75e48d83e6f34b72823995aff6dfbc6bdf92
Dinis Afonso Cerqueira Galvão <up202207217@edu.fe.up.pt>
2025-05-11 21:33:14 +0100

    added screenshots

---
+++ build/CMakeFiles/AED_proj_2.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/master/Documents/School/Bachelor's/Year2/AED/project2/src/Airline.cpp" "CMakeFiles/AED_proj_2.dir/src/Airline.cpp.o" "gcc" "CMakeFiles/AED_proj_2.dir/src/Airline.cpp.o.d"
+  "/home/master/Documents/School/Bachelor's/Year2/AED/project2/src/Airport.cpp" "CMakeFiles/AED_proj_2.dir/src/Airport.cpp.o" "gcc" "CMakeFiles/AED_proj_2.dir/src/Airport.cpp.o.d"
+  "/home/master/Documents/School/Bachelor's/Year2/AED/project2/src/Application.cpp" "CMakeFiles/AED_proj_2.dir/src/Application.cpp.o" "gcc" "CMakeFiles/AED_proj_2.dir/src/Application.cpp.o.d"
+  "/home/master/Documents/School/Bachelor's/Year2/AED/project2/src/FileReader.cpp" "CMakeFiles/AED_proj_2.dir/src/FileReader.cpp.o" "gcc" "CMakeFiles/AED_proj_2.dir/src/FileReader.cpp.o.d"
+  "/home/master/Documents/School/Bachelor's/Year2/AED/project2/src/Graph.cpp" "CMakeFiles/AED_proj_2.dir/src/Graph.cpp.o" "gcc" "CMakeFiles/AED_proj_2.dir/src/Graph.cpp.o.d"
+  "/home/master/Documents/School/Bachelor's/Year2/AED/project2/src/Interface.cpp" "CMakeFiles/AED_proj_2.dir/src/Interface.cpp.o" "gcc" "CMakeFiles/AED_proj_2.dir/src/Interface.cpp.o.d"
+  "/home/master/Documents/School/Bachelor's/Year2/AED/project2/src/main.cpp" "CMakeFiles/AED_proj_2.dir/src/main.cpp.o" "gcc" "CMakeFiles/AED_proj_2.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/AED_proj_2.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "AED_proj_2"
+  "AED_proj_2.pdb"
+  "CMakeFiles/AED_proj_2.dir/src/Airline.cpp.o"
+  "CMakeFiles/AED_proj_2.dir/src/Airline.cpp.o.d"
+  "CMakeFiles/AED_proj_2.dir/src/Airport.cpp.o"
+  "CMakeFiles/AED_proj_2.dir/src/Airport.cpp.o.d"
+  "CMakeFiles/AED_proj_2.dir/src/Application.cpp.o"
+  "CMakeFiles/AED_proj_2.dir/src/Application.cpp.o.d"
+  "CMakeFiles/AED_proj_2.dir/src/FileReader.cpp.o"
+  "CMakeFiles/AED_proj_2.dir/src/FileReader.cpp.o.d"
+  "CMakeFiles/AED_proj_2.dir/src/Graph.cpp.o"
+  "CMakeFiles/AED_proj_2.dir/src/Graph.cpp.o.d"
+  "CMakeFiles/AED_proj_2.dir/src/Interface.cpp.o"
+  "CMakeFiles/AED_proj_2.dir/src/Interface.cpp.o.d"
+  "CMakeFiles/AED_proj_2.dir/src/main.cpp.o"
+  "CMakeFiles/AED_proj_2.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AED_proj_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
